877859834ecf39768c1ca6b0b9266c7f2d03c203
hideki0324 <hideki19850324@gmail.com>
2018-03-09 19:45:32 -0800

    1st up

---
+++ index/index.js
@@ -0,0 +1,56 @@
+//var vid = document.getElementById("text");
+//var vine = document.getElementById("vine");
+//var tree = document.getElementById("tree");
+//var turkey1 = document.getElementById("turkey1");
+//var turkey2 = document.getElementById("turkey2");
+//var cheken = document.getElementById("cheken");
+//var tree = document.getElementById("tree");
+//var cloud = document.getElementById("cloud");
+//var foods = document.getElementById("foods");
+//var tree_b = document.getElementById("b_tree");
+
+
+document.getElementById("open").addEventListener("click", function(){
+    document.getElementById("message").style.left = "0px";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("message").style.left = "-660px";
+})
+
+
+document.getElementById("vine").addEventListener("click", function(){
+    document.getElementById("vine").style.top = "100px";
+    document.getElementById("vine").style.left = "100px";
+})
+
+//tree.addEventListener("click",function(){
+// 
+//});
+//
+//
+document.getElementById("turkey1").addEventListener("click", function(){
+    document.getElementById("vine").style.top = "100px";
+    document.getElementById("vine").style.left = "100px";
+})
+//
+//turkey2.addEventListener("click",function(){
+//  
+//
+//cheken.addEventListener("click",function(){
+//  
+//});
+//
+//tree.addEventListener("click",function(){
+// 
+//});
+//
+//foods.addEventListener("click",function(){
+//  
+//});
+//
+//var button = document.getElementById("button");
+//
+//foods.addEventListener("click",function(){
+// 
+//});
