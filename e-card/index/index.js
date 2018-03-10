f858f426e59f4dc1da154e2231f09d4c5ec6febf
hideki0324 <hideki19850324@gmail.com>
2018-03-09 22:22:37 -0800

    fix html, css, js file

---
+++ e-card/index/index.js
@@ -0,0 +1,215 @@
+var fact1 = document.getElementById("fact1_1_")
+var fact2 = document.getElementById("fact2_1_")
+var fact3 = document.getElementById("fact3_1_")
+var fact4 = document.getElementById("fact4_1_")
+var fact5 = document.getElementById("fact5_1_")
+var fact6 = document.getElementById("fact6_1_")
+var fact7 = document.getElementById("fact7_1_")
+var fact8 = document.getElementById("fact8_1_")
+var fact9 = document.getElementById("fact9_1_")
+var fact10 = document.getElementById("fact10_1_")
+
+
+fact1.onclick = function () {
+    fact1.style.display = "inline";
+    fact1.classList.add("fact1style");
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+}
+
+fact2.onclick = function (){
+    fact2.style.display = "inline";
+    fact2.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact4.style.display = "none";
+    fact3.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+
+}
+
+fact2.onclick = function (){
+    fact3.style.display = "inline";
+    fact3.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact4.style.display = "none";
+    fact2.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+
+}
+
+fact4.onclick = function(){
+    fact3.style.display = "inline";
+    fact3.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact4.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+
+}
+
+fac5.onclick = function(){
+    fact5.style.display = "inline";
+    fact5.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+
+}
+fact6.onclick = function (){
+    fact6.style.display = "inline";
+    fact6.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact5.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+}
+
+fact7.onclick = function (){
+    fact7.style.display = "inline";
+    fact7.classList.add("fact1style");
+    fact1.styl.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+}
+
+fact8.onclick = function (){
+    fact8.style.display = "inline";
+    fact8.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact7.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+}
+
+fact9.onclick = function (){
+    fact9.style.display = "inline";
+    fact9.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact7.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display ="none";
+    fact8.style.display = "none";
+    fact10.style.display = "none";
+}
+
+fact10.onclick = function (){
+    fact10.style.display = "inline";
+    fact10.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact2.style.display = "none";
+    fact3.style.display = "none";
+    fact4.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+}
+
+
+document.getElementById("open").addEventListener("click", function(){
+    document.getElementById("message").style.left = "0px";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("message").style.left = "-660px";
+})
+
+
+document.getElementById("vine").addEventListener("click", function(){
+    document.getElementById("vine").style.top = "100px";
+    document.getElementById("vine").style.left = "100px";
+})
+
+
+//tree.addEventListener("click",function(){
+// 
+//});
+//
+//
+document.getElementById("turkey1").addEventListener("onclick", function(){
+    document.getElementById("turkey1").style.top = "100px";
+    document.getElementById("turkey1").style.left = "100px";
+    fact1.style.display = "inline";
+    fact1.classList.add("fact1style");
+    fact2.style.display = "none";
+    fact4.style.display = "none"
+    fact3.style.display = "none"
+    fact5.style.display = "none"
+    mario.play(); 
+    
+}
+})
+//
+//turkey2.addEventListener("click",function(){
+//  
+//
+//cheken.addEventListener("click",function(){
+//  
+//});
+//
+//tree.addEventListener("click",function(){
+// 
+//});
+//
+//foods.addEventListener("click",function(){
+//  
+//});
+//
+//var button = document.getElementById("button");
+//
+//foods.addEventListener("click",function(){
+// 
+//});
+
+
+
+
+
