4756e288a386ae0e97020fc8421ecc9c971a9956
hideki0324 <hideki19850324@gmail.com>
2018-03-09 23:19:00 -0800

    3rd fix

--- index/index.js
+++ index/index.js
@@ -1,56 +1,179 @@
-//var vid = document.getElementById("text");
-//var vine = document.getElementById("vine");
-//var tree = document.getElementById("tree");
-//var turkey1 = document.getElementById("turkey1");
-//var turkey2 = document.getElementById("turkey2");
-//var cheken = document.getElementById("cheken");
-//var tree = document.getElementById("tree");
-//var cloud = document.getElementById("cloud");
-//var foods = document.getElementById("foods");
-//var tree_b = document.getElementById("b_tree");
+var fact1 = document.getElementById("fact1_1_");
+var fact2 = document.getElementById("fact2_1_");
+var fact3 = document.getElementById("fact3_1_");
+var fact4 = document.getElementById("fact4_1_");
+var fact5 = document.getElementById("fact5_1_");
+var fact6 = document.getElementById("fact6_1_");
+var fact7 = document.getElementById("fact7_1_");
+var fact8 = document.getElementById("fact8_1_");
+var fact9 = document.getElementById("fact9_1_");
+var fact10 = document.getElementById("fact10_1_");
 
+document.getElementById("fact1").addEventListener("click", function(){
+    fact1_1_.style.display = "inline";
+    fact1_1_.classList.add("fact1style");
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+});
+
+document.getElementById("fact2").addEventListener("click", function(){
+    fact2_1_.style.display = "inline";
+    fact2_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+
+});
+
+document.getElementById("fact3").addEventListener("click", function(){
+    fact3.style.display = "inline";
+    fact3.classList.add("fact1style");
+    fact1.style.display = "none";
+    fact4.style.display = "none";
+    fact2.style.display = "none";
+    fact5.style.display = "none";
+    fact6.style.display = "none";
+    fact7.style.display = "none";
+    fact8.style.display = "none";
+    fact9.style.display = "none";
+    fact10.style.display = "none";
+
+});
+
+document.getElementById("fact4").addEventListener("click", function(){
+    fact3_1_.style.display = "inline";
+    fact3_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+
+});
+document.getElementById("fact5").addEventListener("click", function(){
+    fact5_1_.style.display = "inline";
+    fact5_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+
+}
+document.getElementById("fact6").addEventListener("click", function(){
+    fact6_1_.style.display = "inline";
+    fact6_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+});
+
+document.getElementById("fact7").addEventListener("click", function(){
+    fact7_1_.style.display = "inline";
+    fact7_1_.classList.add("fact1style");
+    fact1_1_.styl.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+});
+
+document.getElementById("fact8").addEventListener("click", function(){
+    fact8_1_.style.display = "inline";
+    fact8_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+});
+
+document.getElementById("fact9").addEventListener("click", function(){
+    fact9_1_.style.display = "inline";
+    fact9_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display ="none";
+    fact8_1_.style.display = "none";
+    fact10_1_.style.display = "none";
+});
+
+document.getElementById("fact10").addEventListener("click", function(){
+    fact10_1_.style.display = "inline";
+    fact10_1_.classList.add("fact1style");
+    fact1_1_.style.display = "none";
+    fact2_1_.style.display = "none";
+    fact3_1_.style.display = "none";
+    fact4_1_.style.display = "none";
+    fact5_1_.style.display = "none";
+    fact6_1_.style.display = "none";
+    fact7_1_.style.display = "none";
+    fact8_1_.style.display = "none";
+    fact9_1_.style.display = "none";
+});
 
 document.getElementById("open").addEventListener("click", function(){
     document.getElementById("message").style.left = "0px";
-})
+});
 
 document.getElementById("close").addEventListener("click", function(){
     document.getElementById("message").style.left = "-660px";
-})
+});
 
 
 document.getElementById("vine").addEventListener("click", function(){
     document.getElementById("vine").style.top = "100px";
     document.getElementById("vine").style.left = "100px";
+});
+
+document.getElementById("turkey1").addEventListener("click", function(){
+    document.getElementById("turkey1").style.top = "100px";
+    document.getElementById("turkey1").style.left = "100px";
+    
+}
 })
 
-//tree.addEventListener("click",function(){
-// 
-//});
-//
-//
-document.getElementById("turkey1").addEventListener("click", function(){
-    document.getElementById("vine").style.top = "100px";
-    document.getElementById("vine").style.left = "100px";
-})
-//
-//turkey2.addEventListener("click",function(){
-//  
-//
-//cheken.addEventListener("click",function(){
-//  
-//});
-//
-//tree.addEventListener("click",function(){
-// 
-//});
-//
-//foods.addEventListener("click",function(){
-//  
-//});
-//
-//var button = document.getElementById("button");
-//
-//foods.addEventListener("click",function(){
-// 
-//});
+
+
+
+
+
